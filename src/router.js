cca4c5036a784a9a407402aa09aef52aa9b6c339
Daniel Shokri <danielshokri@gmail.com>
2019-07-11 19:13:17 +0300

    cc

--- src/router.js
+++ src/router.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import SignUp from './views/SignUp.vue'
+import UsersFeed from './views/UsersFeed.vue'
+import UserProfile from './views/UserProfile.vue'
+import Parks from './views/Parks.vue'
+import ParkDetails from './views/ParkDetails.vue'
 
 Vue.use(Router)
 
@@ -8,7 +13,9 @@ export default new Router({
   routes: [
     {path: '/',component: Home},
     {path: '/signup',component: SignUp},
+    {path: '/user',component: UsersFeed},
     {path: '/user/:id',component: UserProfile},
-    {path: '/parks',component: Parks},
+    {path: '/park',component: Parks},
+    {path: '/park/:id',component: ParkDetails},
   ]
 })
