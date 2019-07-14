ffe90a1db7f1e273d0fc7b983da16d4526a942f9
idan <idanelbaz@gmail.com>
2019-07-14 08:41:40 +0300

    with map

--- src/main.js
+++ src/main.js
@@ -3,11 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import * as VueGoogleMaps from 'vue2-google-maps';
 
 Vue.config.productionTip = false
 
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyCrVxVPta_TOsFatlYL7vOx_stAJNlV8ws',
+        libraries: 'places'
+    }
+});
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
