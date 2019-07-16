3d4f1a9dbe9e83e09b81fa654e6248bcd42f1a70
Daniel Shokri <danielshokri@gmail.com>
2019-07-16 12:37:29 +0300

    Hedaer logo change

---
+++ src/components/UsersFilter.cmp.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-input class="search-box" placeholder="Search..." type="search" icon="magnify"></b-input>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.search-box {
+    position: absolute;
+    top: 398px;
+    left: 239px;
+    width: 280px;
+}
+</style>
