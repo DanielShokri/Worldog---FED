93f0823505c5f05c502c820a81d6f0ac983fb697
chen mordechai <chen100030@gmail.com>
2019-07-16 17:29:04 +0300

    chh

--- src/components/DogList.cmp.vue
+++ src/components/DogList.cmp.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
 <v-layout row wrap v-if="dogs">
 <v-flex xs12 sm6 mb4 lg3 v-for="dog in dogs" :key="dog._id">
 
@@ -33,29 +32,6 @@ props: ["dogs", "loggedinUser"],
     components: {
         DogPreview,
 
-=======
-  <v-layout row wrap>
-      <!-- <userFilter></userFilter> -->
-    <v-flex xs12 sm6 mb4 lg3 v-for="dog in dogs" :key="dog._id">
-      <dog-preview :dog="dog" @delete="emitDeleteDog"></dog-preview>
-    </v-flex>
-  </v-layout>
-</template>
-
-<script>
-import DogPreview from "./DogPreview.cmp.vue";
-import userFilter from './UsersFilter.cmp'
-export default {
-  props: {
-    dogs: Array
-  },
-  data() {
-    return {};
-  },
-  methods: {
-    emitDeleteDog(dogId) {
-      this.$emit("delete", dogId);
->>>>>>> 0bdbb19cf539365ba44ade02250be171cccfa57f
     }
   },
   components: {
